5323da43095bdf5b63346292654834b832500e58
yalamanchichandra <202117b3778@wilp.bits-pilani.ac.in>
2026-02-06 18:35:23 +0530

    Delete tfidf.py

--- tfidf.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-
-def compute_tfidf_similarity(jd_text: str, resume_text: str) -> float:
-    vectorizer = TfidfVectorizer(stop_words="english")
-    tfidf = vectorizer.fit_transform([jd_text, resume_text])
-    similarity = cosine_similarity(tfidf[0:1], tfidf[1:2])[0][0]
-    return round(similarity * 100, 2)
